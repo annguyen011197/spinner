8eecda3e36ea3024caa4113ae8e77cf17d82b947
An Nguyen <tranminhannguyen@gmail.com>
2019-09-22 07:16:46 +0700

    Updates

---
+++ precache-manifest.f88031f06cf92d1b7495f9aafa408991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa9bd3f82270805b2740007cae391af",
+    "url": "/spinner/index.html"
+  },
+  {
+    "revision": "7dbefae92528044bb5eb",
+    "url": "/spinner/static/css/main.600324b9.chunk.css"
+  },
+  {
+    "revision": "bebba661cc717ac2b079",
+    "url": "/spinner/static/js/2.1e1e4689.chunk.js"
+  },
+  {
+    "revision": "7dbefae92528044bb5eb",
+    "url": "/spinner/static/js/main.a8ecd611.chunk.js"
+  },
+  {
+    "revision": "f8587d77301516128b84",
+    "url": "/spinner/static/js/runtime-main.a009fbc2.js"
+  },
+  {
+    "revision": "6786e3501bcaf65660c97d066f2dd23e",
+    "url": "/spinner/static/media/12077.6786e350.png"
+  },
+  {
+    "revision": "b81cc1b74cf4d9367c6e6bff4f4312a0",
+    "url": "/spinner/static/media/settings-gears.b81cc1b7.png"
+  }
+]);
